770bdb3062713ef173e8f9048e3f47643f1f1dc8
Kenderly Delinois <kenderly.delinois1@outlook.com>
2025-05-14 16:56:35 -0500

    Add project files.

---
+++ Week_6_Assignment_6.3.1/CustomersQueue.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_6_Assignment_6._3._1
+{
+    class Caller
+    {
+        public string LName;
+        public string FName;
+        public int TicketNum;
+
+        public Caller(string lName, string fName, int ticketNum)
+        {
+            LName = lName;
+            FName = fName;
+            TicketNum = ticketNum;
+        }
+        public override string ToString()
+        {
+            return ($"{FName} {LName} (Ticket: {TicketNum})");
+        }
+    }
+    internal class CustomersQueue
+    {
+        public Caller Caller;
+        public CustomersQueue next;
+
+        public CustomersQueue(Caller Caller, CustomersQueue next)
+        {
+            this.Caller = Caller;
+            this.next = next;
+        }
+    }
+
+    class CallerQueue
+    {
+        CustomersQueue front;
+        CustomersQueue rear;
+        int size;
+
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+        }
+
+        public void Enqueue(Caller Caller)
+        {
+            CustomersQueue queue = new CustomersQueue(Caller , null);
+            if(IsEmpty())
+            {
+                front = queue;
+            }
+            else
+            {
+                rear.next = queue;
+            }
+            rear = queue;
+            size++;
+        }
+        public void Dequeue()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            Caller Caller = front.Caller;
+            front = front.next;
+            size--;
+            if(IsEmpty())
+            {
+                rear = null;
+            }
+            return;
+        }
+
+        public void Display()
+        {
+            CustomersQueue temp = front;
+            if(!IsEmpty())
+            {
+                while(temp != null)
+                {
+                    Console.WriteLine(temp.Caller + " ");
+                    temp = temp.next;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Q is empty");
+            }
+        }
+    }
+}
---
+++ Week_6_Assignment_6.3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Week_6_Assignment_6._3._1
+{
+    internal class Program
+    {
+        //You are developing a program to manage a call queue of customers 
+        //using the Queue  in C#. The program creates a queue of callers and 
+        //demonstrates the functionality of enqueueing elements into the queue 
+        //and iterating over the elements and dequeuing.
+
+        //Use linked lists
+
+        static void Main(string[] args)
+        {
+            List<Caller> callerlist = new List<Caller>();
+            CallerQueue callerQueue = new CallerQueue();
+            Caller newCaller = new Caller("Joseph", "Kenny", 1);
+            Caller newCaller_2 = new Caller("Doe", "John", 2);
+            Caller newCaller_3 = new Caller("Noel", "Kia", 3);
+            Caller newCaller_4 = new Caller("Bush", "Kelly", 4);
+            Caller newCaller_5 = new Caller("Jeffrey", "Oscar", 5);
+            callerQueue.Enqueue(newCaller);
+            callerQueue.Enqueue(newCaller_2);
+            callerQueue.Enqueue(newCaller_3);
+            callerQueue.Enqueue(newCaller_4);
+            callerQueue.Enqueue(newCaller_5);
+            callerQueue.Dequeue(); // DeQueue the first caller 
+            callerQueue.Display();
+        }
+    }
+}
